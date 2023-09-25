07caf3fafefab15421547b7229884bea4782ec08
AdeTiDotun <adejumodaniel274@gmail.com>
2023-09-25 13:17:15 +0100

    variables

---
+++ intro_javascript/1_variables/README.md
@@ -0,0 +1,2 @@
+Lectures and tasks on variables on Variables.
+The values of variables are swapped.
---
+++ intro_javascript/1_variables/variable.js
@@ -0,0 +1,9 @@
+var a = 8;
+var b = 3;
+
+var c = a;
+a = b;
+b = c;
+
+console.log ("a is " + a);
+console.log ("b is " + b);
