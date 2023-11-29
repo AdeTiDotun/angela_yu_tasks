1f50e08f72b774f80c9e139a1dcc58dd30d3c772
AdeTiDotun <adejumodaniel274@gmail.com>
2023-11-29 15:40:48 +0100

    random love number generation

---
+++ intermediate_javascript/love.js
@@ -0,0 +1,7 @@
+firstName = prompt("First name");
+secondName = prompt("Second Name");
+var n = Math.random();
+n = n * 100;
+n = Math.floor(n) + 1;
+
+console.log("The Love percentage of " + firstName + " and " + secondName + " is " + n + " %");
